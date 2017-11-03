136f579dd447842df8a612dbf6d5c182a0d688ee
Eslam El-Meniawy <eslam@Eslams-Mac.local>
2017-11-03 11:50:44 +0200

    Initial Commit

---
+++ MovieDbPopularPeople/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDbPopularPeople
+//
+//  Created by Eslam El-Meniawy on 11/3/17.
+//  Copyright © 2017 Eslam El-Meniawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
